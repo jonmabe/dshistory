caeb358c5d4bbb630f716209ee18a048479d5a36
amattie <amattie@65b74cb0-b242-0410-8c42-fd9a00cf7358>
2008-02-20 02:47:10 +0000

    fix for safari 3 -- supportsChangingHistoryViaFrame wasn't being consumed in all circumstances

--- dshistory.js
+++ dshistory.js
@@ -1,27 +1,10 @@
 /*!
-dsHistory, v1-beta1 $Rev$
-Revision date: $Date$
-Project URL: http://code.google.com/p/dshistory/
-
-Copyright (c) Andrew Mattie (http://www.akmattie.net)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+ * dsHistory, v1-beta1 $Rev$
+* Revision date: $Date$
+* Project URL: http://code.google.com/p/dshistory/
+* 
+* Copyright (c) Andrew Mattie (http://www.akmattie.net)
+* Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
 */
 
 var dsHistory = function() {
